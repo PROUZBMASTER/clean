562c57db9ab40f9258fe99f6289a69c39a6bd136
Azizbek <PROUZBMASTER>
2023-03-25 13:31:50 +0500

    fix

---
+++ index.js
@@ -0,0 +1,34 @@
+let ul = document.querySelector(".ulh3")
+let form = document.querySelector(".form")
+let input = document.querySelector(".form-control")
+let clear = document.querySelector(".btn-delete")
+
+let todos = JSON.parse(localStorage.getItem("list")) || []
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const inputText = input.value
+    form.reset()
+    todos.push({ name: inputText })
+    setTodos()
+    showTodos()
+})
+function setTodos() {
+    localStorage.setItem("list", JSON.stringify(todos))
+}
+function showTodos() {
+    const todos = JSON.parse(localStorage.getItem("list"))
+    ul.innerHTML = ''
+    todos.forEach((elem) => {
+        ul.innerHTML += `
+        <h2 class="text">${elem.name}</h2>
+        `
+    })
+}
+
+clear.onclick = () => {
+    localStorage.setItem("list", JSON.stringify([]))
+    todos = []
+    setTodos()
+    showTodos()
+}
